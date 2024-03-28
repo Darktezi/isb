a0c4fb93dd6122a26750410b5b5323de8f44534c
Darktezi <darktezi@mail.ru>
2024-03-28 17:43:52 +0400

    third test ready

--- lab_2/test.py
+++ lab_2/test.py
@@ -1,6 +1,10 @@
 import os
 import json
 import math
+import mpmath
+
+
+pi = {0: 0.2148, 1: 0.3672, 2: 0.2305, 3: 0.1875}
 
 
 def frequency_test(sequence):
@@ -22,9 +26,42 @@ def identical_consecutive_bits(sequence):
     return p_value
 
 
+def longest_sequence_test(sequence):
+    i_seq = list(map(int, sequence))
+    block_size = 8
+    blocks = [i_seq[i:i + block_size] for i in range(0, len(i_seq), block_size)]
+    v = {1: 0, 2: 0, 3: 0, 4: 0}
+    for block in blocks:
+        max_seq = 0
+        temp_max = 0
+        for bit in block:
+            temp_max = (temp_max + 1) if bit == 1 else 0
+            max_seq = max(max_seq, temp_max)
+        match max_seq:
+            case 0 | 1:
+                v[1] += 1
+            case 2:
+                v[2] += 1
+            case 3:
+                v[3] += 1
+            case 4 | 5 | 6 | 7 | 8:
+                v[4] += 1
+            case _:
+                pass
+
+    x_square = 0
+    for i in range(4):
+        x_square += math.pow(v[i + 1] - 16 * pi[i], 2) / (16 * pi[i])
+    p_value = mpmath.gammainc(3/2, x_square/2)
+    
+    return p_value
+
+
 if __name__ == "__main__":
     sequence = "10101010101010001101000000000101101110011101100010010001101001101000011000011110100101011111010111101110010011001011001010111111"
     p_value_frequency = frequency_test(sequence)
     p_value_bits = identical_consecutive_bits(sequence)
+    p_value_longest = longest_sequence_test(sequence)
     print("P-value for Frequency Test:", p_value_frequency)
     print("P-value for bits Test:", p_value_bits)
+    print("P-value for longest sequence of ones Test:", p_value_longest)
